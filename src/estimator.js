1a6390159d046ee1ed9ebf93a5d96aeaf7710605
johnerry <fashanutosin7@gmail.com>
2020-04-12 16:23:04 +0100

    Start of building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input,
+        impact: {},
+        severeImpact: {}
+    };
+};
 
 export default covid19ImpactEstimator;
