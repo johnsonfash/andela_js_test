5b11d5ef619a0bf42e3008b881a1aadc3e1abc86
johnerry <fashanutosin7@gmail.com>
2020-04-13 21:08:52 +0100

    check 23

--- src/estimator.js
+++ src/estimator.js
@@ -61,11 +61,9 @@ const covid19ImpactEstimator = (data) => {
   * severeImpact.infectionsByRequestedTime);
 
   impact.dollarsInFlight = Math.floor(impact.infectionsByRequestedTime
-  * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD)
-  * input.timeToElapse;
+  * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD);
   severeImpact.dollarsInFlight = Math.floor(severeImpact.infectionsByRequestedTime
-  * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD)
-  * input.timeToElapse;
+  * input.region.avgDailyIncomePopulation * input.region.avgDailyIncomeInUSD);
 
   return {
     data: input,
